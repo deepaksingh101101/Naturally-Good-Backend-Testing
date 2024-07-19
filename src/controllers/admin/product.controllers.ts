0fe9057526237d0ae1f618fefd31dea6756cb849
M Fareed <168064282+fareed10786@users.noreply.github.com>
2024-07-19 17:34:50 +0530

    Create Get API of Product

--- src/controllers/admin/product.controllers.ts
+++ src/controllers/admin/product.controllers.ts
@@ -16,3 +16,51 @@ export const createProduct = async (req: Request, res: Response) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 };
+
+
+export const getAllProducts = async (req: Request, res: Response) => {
+    try {
+        const currentpage = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 10;
+        const skip = (currentpage - 1) * limit;
+
+        const products = await ProductModel.find().skip(skip).limit(limit);
+        const total = await ProductModel.countDocuments();
+        const totalPages = Math.ceil(total / limit);
+
+        const prevPage = currentpage > 1;
+        const nextPage = currentpage < totalPages;
+
+        res.status(200).json({
+            total,
+            currentpage,
+            totalpages: totalPages,
+            prevPage,
+            nextPage,
+            products
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+
+export const getProductById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+            return res.status(400).json({ error: 'Invalid product ID format' });
+        }
+
+        const product = await ProductModel.findById(id);
+
+        if (!product) {
+            return res.status(404).json({ error: 'Product not found' });
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { adminMiddleware } from '../middleware/adminIdMiddleware';
-import { createProduct } from '../controllers/admin/product.controllers';
+import { createProduct, getAllProducts, getProductById } from '../controllers/admin/product.controllers';
 
 const router = Router();
 
 router.post('/create', adminMiddleware, createProduct);
+router.get('/allproduct' , getAllProducts)
+router.get('/products/:id', getProductById)
 
 export default router;
