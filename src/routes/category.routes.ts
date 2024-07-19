228943e6ccc3c207eeb6066240b372f46ecde841
Deepak Singh <deepaksingh101101@gmail.com>
2024-07-19 16:14:27 +0530

    CRUD of category Completed

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,9 +1,15 @@
-import { Router } from 'express';
-import { createCategory } from '../controllers/admin/category.controller';
-import { adminMiddleware } from '../middleware/adminIdMiddleware';
+import { superAdminMiddleware } from './../middleware/superadmin.middleware';
+import { Router, Request, Response } from "express";
+import { createCategory, deleteCategory, getAllCategories, getCategoryById, updateCategory } from "../controllers/admin/category.controllers";
+
 
 const router = Router();
 
-router.post('/create', adminMiddleware, createCategory);
+
+router.post('/create', superAdminMiddleware,createCategory);
+router.get('/getAll', getAllCategories);
+router.delete('/delete/:id',deleteCategory );
+router.patch('/update/:id', updateCategory);
+router.get('/:id', getCategoryById);
 
 export default router;
