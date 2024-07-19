7205698862f1ff28c317797aa83252031c3f6026
Deepak Singh <deepaksingh101101@gmail.com>
2024-07-19 16:20:36 +0530

    admin middleware added to routes of category

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,15 +1,15 @@
-import { superAdminMiddleware } from './../middleware/superadmin.middleware';
 import { Router, Request, Response } from "express";
 import { createCategory, deleteCategory, getAllCategories, getCategoryById, updateCategory } from "../controllers/admin/category.controllers";
+import { adminMiddleware } from '../middleware/adminIdMiddleware';
 
 
 const router = Router();
 
 
-router.post('/create', superAdminMiddleware,createCategory);
-router.get('/getAll', getAllCategories);
-router.delete('/delete/:id',deleteCategory );
-router.patch('/update/:id', updateCategory);
-router.get('/:id', getCategoryById);
+router.post('/create', adminMiddleware,createCategory);
+router.get('/getAll',adminMiddleware, getAllCategories);
+router.delete('/delete/:id',adminMiddleware,deleteCategory );
+router.patch('/update/:id',adminMiddleware, updateCategory);
+router.get('/:id',adminMiddleware, getCategoryById);
 
 export default router;
