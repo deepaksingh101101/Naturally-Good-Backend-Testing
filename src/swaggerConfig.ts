a4cc4875b2072cb2d280bcdd215fa0d8c75c68ad
Deepak Singh <deepaksingh101101@gmail.com>
2024-08-05 17:20:48 +0530

    swagger api setuped

--- src/swaggerConfig.ts
+++ src/swaggerConfig.ts
@@ -6,11 +6,11 @@ import path from 'path';
 export const setupSwagger = (app: express.Application) => {
   // Load multiple Swagger documents
   const swaggerDocuments = {
+    admin: YAML.load(path.join(__dirname, './swagger-docs/admin/SuperAdmin.yaml')),
+    // Uncomment and add other documents as needed
     // tasks: YAML.load(path.join(__dirname, './swagger-docs/Tasks.yaml')),
     // auth: YAML.load(path.join(__dirname, './swagger-docs/Auth.yaml')),
-    // admin: YAML.load(path.join(__dirname, './swagger-docs/Admin.yaml')),
-    admin: YAML.load(path.join(__dirname, './swagger-docs/admin/SuperAdmin.yaml')),
-    user: YAML.load(path.join(__dirname, './swagger-docs/user/User.yaml')),
+    // user: YAML.load(path.join(__dirname, './swagger-docs/user/User.yaml')),
     // category: YAML.load(path.join(__dirname, './swagger-docs/Category.yaml')),
     // product: YAML.load(path.join(__dirname, './swagger-docs/Product.yaml')),
     // plan: YAML.load(path.join(__dirname, './swagger-docs/Plan.yaml')),
@@ -18,7 +18,9 @@ export const setupSwagger = (app: express.Application) => {
 
   // Set up Swagger UI for each document
   for (const [key, document] of Object.entries(swaggerDocuments)) {
+    // Use Swagger UI to serve the documentation
     app.use(`/docs/${key}`, swaggerUi.serve, swaggerUi.setup(document));
+    // Endpoint to get the raw Swagger JSON document
     app.get(`/docs/${key}.json`, (req, res) => {
       res.setHeader('Content-Type', 'application/json');
       res.send(document);
