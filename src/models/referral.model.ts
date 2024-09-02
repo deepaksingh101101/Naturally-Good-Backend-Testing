4ec91ab8f1ab7d127e7523f7c99e4714efb943da
Deepak Singh <deepaksingh101101@gmail.com>
2024-09-02 13:09:24 +0530

    refrerall model and coupons model done going for customer

---
+++ src/models/referral.model.ts
@@ -0,0 +1,32 @@
+import { getModelForClass, pre, prop, Ref } from '@typegoose/typegoose';
+import { User } from './user.model';
+import { Subscription } from './subscription.model';
+import { Order } from './order.model';
+import { Coupon } from './coupons.model';
+
+@pre<Referral>('save', async function () {
+  this.UpdatedAt = new Date();
+})
+class Referral {
+
+  @prop({ ref: () => User, required: true })
+  public ReferredBy!: Ref<User>;
+
+  @prop({ ref: () => User })
+  public ReferredTO!: Ref<User>;
+
+  @prop({ type: Date, default: Date.now })
+  public UpdatedAt!: Date;
+
+  @prop({ type: Date, default: Date.now })
+  public CreatedAt!: Date;
+
+  @prop({ ref: () => Subscription })
+  public SubscriptionPurchased!: Ref<Subscription>;
+
+
+}
+
+const ReferralModel = getModelForClass(Referral);
+
+export default ReferralModel;
