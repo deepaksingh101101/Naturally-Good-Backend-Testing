5ab6f23780faf7f8bfd856a10610e9d35725d3ec
Deepak Singh <deepaksingh101101@gmail.com>
2024-09-05 12:33:49 +0530

    order model done

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -8,12 +8,14 @@ import { Delivery } from './delivery.model';
 export enum PaymentType {
   Cash = 'cash',
   Card = 'card',
-  Online = 'online',
+  Upi = 'upi',
+  NetBanking = 'netbanking',
 }
 
 export enum PaymentStatus {
   Paid = 'paid',
   Pending = 'pending',
+  Unpaid = 'unpaid',
 }
 
 @pre<Order>('save', async function() {
@@ -24,11 +26,23 @@ export class Order {
   @prop({ ref: () => User, required: true })
   public UserId!: Ref<User>;
 
-  // @prop({ ref: () => Subscription, required: true })
-  // public SubscriptionId!: Ref<Subscription>;
+  @prop({ ref: () => Subscription, required: true })
+  public SubscriptionId!: Ref<Subscription>;
 
   @prop({ type: Number, required: true })
   public NetPrice!: number;
+
+  @prop({ ref: () => Coupon })
+  public Coupons?: Ref<Coupon>;
+
+  @prop({ type: Number ,default:0})
+  public ManualDiscountPercentage?: number;
+
+  @prop({ type: Number, required: true,default:0 })
+  public AmountReceived!: number;
+
+  @prop({ type: Number, required: true ,default:0})
+  public AmountDue!: number;
 
   @prop({ type: Date, required: true })
   public BookingDate!: Date;
@@ -39,26 +53,17 @@ export class Order {
   @prop({ type: Date })
   public PaymentDate?: Date;
 
-  @prop({ type: String, enum: PaymentType, required: true })
-  public PaymentType!: PaymentType;
-
   @prop({ type: String, enum: PaymentStatus, required: true })
   public PaymentStatus!: PaymentStatus;
+
+  @prop({ type: String, enum: PaymentType, required: true })
+  public PaymentType!: PaymentType;
 
   @prop({ type: String })
   public SpecialInstruction?: string;
 
-  @prop({ ref: () => Coupon })
-  public Coupons?: Ref<Coupon>;
-
-  @prop({ type: Number })
-  public ManualDiscountPercentage?: number;
-
-  @prop({ type: Number, required: true,default:0 })
-  public AmountReceived!: number;
-
-  @prop({ type: Number, required: true ,default:0})
-  public AmountDue!: number;
+  @prop({ type: Boolean,default:false })
+  public isCurrentOrder!: boolean;
 
   @prop({ ref: () => Delivery })
   public Deliveries!: Ref<Delivery>[];
