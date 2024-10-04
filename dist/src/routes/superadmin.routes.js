e669d6604c9b13a53041592978c008bb9168caa9
Deepak Singh <deepaksingh101101@gmail.com>
2024-10-04 11:30:33 +0530

    first commit

--- dist/src/routes/superadmin.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const superadmin_controller_1 = require("../controllers/auth/superadmin.controller");
-const router = (0, express_1.Router)();
-router.post('/create', superadmin_controller_1.createSuperAdmin);
-router.post('/login', superadmin_controller_1.loginSuperAdmin);
-exports.default = router;
--- dist/src/utils/category/category.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CategoryType = void 0;
-var CategoryType;
-(function (CategoryType) {
-    CategoryType["Marrow"] = "Marrow";
-    CategoryType["EdiblePlantStem"] = "Edible plant stem";
-    CategoryType["Allium"] = "Allium";
-})(CategoryType || (exports.CategoryType = CategoryType = {}));
